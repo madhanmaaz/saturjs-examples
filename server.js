a56ef84e6d798a705a8ba1c563ffa557edebdbf8
Madhan <120784875+madhanmaaz@users.noreply.github.com>
2024-11-03 23:04:00 +0530

    new

--- server.js
+++ server.js
@@ -1,30 +1,11 @@
-const express = require("express")
-const saturjs = require("saturjs")
+const { setup, Router, renderPage } = require("saturjs")
+const router = Router()
 
-const app = express()
-app.use(express.static(__dirname + "/public"))
-saturjs.setup(app, {
-    dev: true,
-    version: require("./package.json").version
+router.get("/", renderPage("index"))
+router.get("/todo", renderPage("todo/index"))
+router.get("/counter", renderPage("counter/index"))
+router.get("/image-gallery", renderPage("imageGallery/index"))
+
+module.exports = setup({
+    appRouter: router,
 })
-
-app.get("/", (req, res) => {
-    res.render("home")
-})
-
-app.get("/counter", (req, res) => {
-    res.render("counter/Main")
-})
-
-app.get("/todo", (req, res) => {
-    res.render("todo/Main")
-})
-
-app.get("/image-gallery", (req, res) => {
-    res.render("imageGallery/Main", {
-        data: require("./imageData")
-    })
-})
-
-saturjs.errorHandler(app)
-app.listen(3000)
